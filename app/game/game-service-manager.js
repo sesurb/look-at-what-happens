64f0d9b93aafdab0c90fddba5f7b2e7f51a9194c
Jake Overall <joverall22@gmail.com>
2016-03-02 09:37:14 -0700

    adds comments for student challenge

--- app/game/game-service-manager.js
+++ app/game/game-service-manager.js
@@ -13,6 +13,8 @@ app.service('GameManager', function(CharactersService){
 	}
 	
 	this.checkGuess = function (character) {
+        //THIS ALLOWS YOU TO CHECK EACH CHARACTER INDIVIDUALLY
+        //NO MODIFICATION NEEDED HERE
 		if(gameOver()){ return }
 		if(character === _computerChoice){
 			_game.victory = true;
@@ -24,64 +26,64 @@ app.service('GameManager', function(CharactersService){
 	
 	this.checkProperty = function(prop){
 		if(gameOver()){ return }
-		if(_game.traitsCost >= 10 - _game.guesses){
-			_game.message = "Sorry you don't have enough points for that."
-			return
-		}
-		_game.traitsCost += 1; 
+        
+        /**  CAN GUESS
+         * Check if the traitCost is greater than remaning guesses
+         * totalGuesses = 10; guesses starts at 0 and should be for each guess
+         * the traitCost starts at 2 and goes up by one for each propertyCheck
+         * Dont forget to add the traitCost to the _game.guesses
+         * setting _game.message will provide the user with feedback
+         */
+        
+        /**  _COMPUTERCHOICE has TRAIT
+         * _computerChocie.traits === [String, String]
+         * check if _computerChoice.traits has the selected prop.name if so set hasProp = true;
+         * also set prop.used = true to disable the same trait check
+         */
+		
 		var hasProp = false;
 		var found = false;
-		prop.used = true;
-		_computerChoice.traits.forEach(function(trait){
-			if(trait === prop.name){
-				hasProp = true;
-			}
-		})
-		_game.characters.forEach(function(character) {
-			found = false;
-			character.traits.forEach(function(trait) {
-				if(trait === prop.name){
-					found = true;
-				}
-			});
-			if(hasProp && !found || !hasProp && found){
-				character.possible = false;
-			}
-		});
-		_game.guesses += _game.traitsCost;
+        
+        /** EACH CHARACTER HAS TRAIT
+         * now check each _game.characters individually
+         * if the character.traits has prop.name set 
+         * found = true
+         * after checking the traits
+         *  
+         * if hasProp && !found 
+         * character.possible = false
+         * 
+         * What else would cause a character.possible to === false?
+         * 
+         */
 	}
 	
 	function reset(){
-		_game.guesses = 0;
-		_game.victory = false;
-		_game.gameOver = false;
-		_game.traitsCost = 2;
-		_game.message = '';
-		_game.computerChoice = '';
-		_game.characters.forEach(function(character){
-			character.possible = true;
-		});
-		for(var prop in _game.propertyList){
-			_game.propertyList[prop].used = false;
-		}
+        /**
+         * Reset all of the values on _game
+         * each character on _game.characters should set to 
+         * character.possible = true
+         * all of the traits in  _game.propertyList <--- its an {}
+         * should be set to 
+         * _game.propertyList[trait].used = false
+         */
 	}
 	
 	function gameOver() {
-		if(_game.guesses > 9){
-			_game.gameOver = true;
-			_game.message = 'HAHAHA the computer Wins';
-			return true;
-		}
-		if(_game.victory){
-			_game.computerChoice = _computerChoice;
-			_game.message = 'Bummer you have beaten the computer.';
-			return true;
-		}
+		/**
+         * make sure the guesses are less than 10 
+         * return true if the game should be over
+         * if the game.victory 
+         * set _game.computerChoice = _computerChoice
+         * return true
+         */
 	}
 	
 	function setRandomChoice(){
-		var randI = Math.floor(Math.random() * _game.characters.length);
-		_computerChoice = _game.characters[randI];
+        /**
+         * This function should get a random index between 0 - _game.characters.length
+         * then set _computerChoice to the object at the randI index
+         */
 	}
 	
 })
